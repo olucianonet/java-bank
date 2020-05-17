41f333370bec19d634048bba8b4bfa190703437a
Luciano <oluciano.net@gmail.com>
2020-05-17 16:18:18 -0300

    03. Entendendo Polimorfismo

---
+++ java-heranca/src/TestaControlaBonificacao.java
@@ -0,0 +1,20 @@
+
+public class TestaControlaBonificacao {
+
+	public static void main(String[] args) {
+
+		Gerente f1 = new Gerente("Joao", "123", 5000.0);
+		Supervisor s1 = new Supervisor("Carlos", "321", 4000.0);
+		Analista a1 = new Analista("Paulo", "432", 3000.0);
+		
+		ControlaBonificacao cb = new ControlaBonificacao();
+		cb.registra(f1);
+		cb.registra(s1);
+		cb.registra(a1);
+		
+		System.out.println("Total de bonificações: " + cb.getSoma());
+	
+		
+	}
+
+}
