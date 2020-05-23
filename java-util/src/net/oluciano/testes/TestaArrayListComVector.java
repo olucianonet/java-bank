dd9e07c5b4e74490cd5a5325a41f76c8da8c30b1
Luciano <oluciano.net@gmail.com>
2020-05-23 14:46:29 -0300

    05. Vector e a interface Collection

--- java-util/src/net/oluciano/testes/TestaArrayListComVector.java
+++ java-util/src/net/oluciano/testes/TestaArrayListComVector.java
@@ -1,16 +1,19 @@
 package net.oluciano.testes;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Vector;
 
 import net.oluciano.modelos.Conta;
 import net.oluciano.modelos.ContaCorrente;
 import net.oluciano.modelos.ContaPoupanca;
 
-public class TestaArrayListComGenerics {
+public class TestaArrayListComVector {
 
 	public static void main(String[] args) {
 
-		ArrayList<Conta> al = new ArrayList<Conta>();
+		// Testando com Vector.
+//		List<Conta> al = new Vector<Conta>();
+		Collection<Conta> al = new Vector<Conta>();
 		
 		Conta c1 = new ContaCorrente(1);
 		c1.deposita(199.0);
@@ -18,16 +21,18 @@ public class TestaArrayListComGenerics {
 		c2.deposita(299.0);
 		Conta c3 = new ContaPoupanca(3);
 		c3.deposita(99.0);
-		// Cria outra conta com os mesmos atributos
 		Conta c4 = new ContaPoupanca(3);
 		c4.deposita(99.0);
 		Conta c5 = new ContaPoupanca(5);
-		c5.deposita(99.0);
+		c5.deposita(99.0);		
 		
 		al.add(c1);
 		al.add(c2);
-//		al.add(c3);
 		al.add(c4);
+		al.remove(c2);
+		
+		// Esse método não é implementado por Collection
+		//System.out.println(al.get(0));
 		
 		if (al.contains(c3)){
 			System.out.println("Conta localizada: " + c3);
@@ -35,15 +40,7 @@ public class TestaArrayListComGenerics {
 		
 		if (al.contains(c5)){
 			System.out.println("Conta localizada: " + c5);
-		}
-
-		
-		
-//		for (Conta conta : al) {
-//			 
-//			System.out.println(conta.meuToString());
-//		}		
-		
+		}		
 	}
 
 }
